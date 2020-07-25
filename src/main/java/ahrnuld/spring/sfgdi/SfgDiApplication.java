4431cb3f5e68c67b12bd022026ead22347e3add7
Mark <mark@starwave.nl>
2020-07-25 16:16:10 +0200

    Added profiles example code

--- src/main/java/ahrnuld/spring/sfgdi/SfgDiApplication.java
+++ src/main/java/ahrnuld/spring/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package ahrnuld.spring.sfgdi;
 
 import ahrnuld.spring.sfgdi.controllers.ConstructorInjectedController;
+import ahrnuld.spring.sfgdi.controllers.I18nController;
 import ahrnuld.spring.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,10 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController)ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
 
 		MyController myController = (MyController)ctx.getBean("myController");
 		String greeting = myController.sayHello();
